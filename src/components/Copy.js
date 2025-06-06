08cd869c826191fa36880bfd0c248675e857cf6f
DaikiARAI20030526 <araidaiki2003@gmail.com>
2025-06-06 14:22:03 +0900

    test

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -3,14 +3,23 @@ import styles from "../styles/Copy.module.css"; // CSSモジュールをイン�
 
 const Copy = () => {
   return (
-<p className={styles.copy} style={{ marginBottom: "36px" }}>
-  かけて！煮て！<br />
-  混ぜて！焼いて！<br />
-  漬けて！炊いて！<br />
-  YOKOYAMAは<span className={styles.break}><br /></span>万能ソースの専門メーカーです。
-</p>
+    <p className={styles.copy} style={{ marginBottom: "36px" }}>
+      かけて！煮て！<br />
+      混ぜて！焼いて！<br />
+      漬けて！炊いて！<br />
+      YOKOYAMAは
+      {/* 既存の1300px以下で改行させる部分 */}
+      <span className={styles.break}>
+        <br />
+      </span>
+      万能ソースの
+      {/* こちらが600px以下でだけ改行を表示する部分 */}
+      <span className={styles.mobileBreak}>
+        <br />
+      </span>
+      専門メーカーです。
+    </p>
   );
 };
 
 export default Copy;
-//
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,5 +1,5 @@
 // src/components/Top.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/Top.module.css";
 
@@ -23,10 +23,40 @@ const images = [
 
 const Top = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 450);
+
+  // ウィンドウサイズ変更を監視して、450px 以下かどうかを判定
+  useEffect(() => {
+    const onResize = () => {
+      setIsMobile(window.innerWidth <= 450);
+    };
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
 
   return (
     <div className={styles.topMain}>
       {images.map((img, index) => {
+        // 450px 以下では常に front のみをレンダリング
+        if (isMobile) {
+          return (
+            <Link
+              key={index}
+              to="/products"
+              className={styles.imageContainer}
+              // モバイルでは hover イベントを使わないので onMouseEnter/Leave は設定しない
+            >
+              <img
+                className={styles.image}
+                src={img.front}
+                alt="TONGARI Front"
+                style={{ width: "100%" }}
+              />
+            </Link>
+          );
+        }
+
+        // それ以外（451px 以上）は、従来どおりホバー判定を行う
         const isHovered = hoveredIndex === index;
         const anyHovered = hoveredIndex !== null;
 
@@ -40,7 +70,7 @@ const Top = () => {
           >
             {isHovered ? (
               <>
-                {/* ホバーされた場合、frontとbackを横並びで表示 */}
+                {/* ホバーされたアイテム：front と back を横並びで表示 */}
                 <img
                   className={styles.image}
                   src={img.front}
@@ -55,7 +85,7 @@ const Top = () => {
                 />
               </>
             ) : anyHovered ? (
-              // 他のアイテムには hover 画像を表示
+              /* ほかのアイテム：hover 用の “薄め” 画像を表示 */
               <img
                 className={styles.image}
                 src={img.hover}
@@ -63,7 +93,7 @@ const Top = () => {
                 style={{ width: "100%" }}
               />
             ) : (
-              // どれもホバーされていないときは front を表示
+              /* まだ何もホバーしていない状態：front を表示 */
               <img
                 className={styles.image}
                 src={img.front}
